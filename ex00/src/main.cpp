319da222eb445526a220dd1a6947a4822e4303d8
Manuel Mazunga De Almeida <maalmeid@c1r1s3.42luanda.com>
2025-05-20 18:26:19 +0100

    redo

---
+++ ex00/src/main.cpp
@@ -0,0 +1,16 @@
+#include "Zombie.hpp"
+
+Zombie* newZombie(std::string name);
+void randomChump(std::string name);
+
+int main()
+{
+    Zombie* heapZombie = newZombie("Heap Zombie");
+    heapZombie->announce();
+
+    randomChump("Stack Zombie");
+
+    delete heapZombie;
+
+    return 0;
+}
