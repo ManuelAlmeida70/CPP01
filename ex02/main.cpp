b5c1e1499cbc099924d6d715716c7bd4f9f3ec75
Manuel Mazunga De Almeida <maalmeid@c1r1s10.42luanda.com>
2025-05-27 17:03:07 +0100

    main done

---
+++ ex02/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+    std::string string = "HI THIS IS BRAIN";
+    std::string* stringPTR = &string;
+    std::string& stringREF = string;
+
+    std::cout << "Endereco da string: " << &string;
+    std::cout << "Endereco contido em stringPTR: " << std::endl;
+    std::cout << "Endereco contido em stringREF: " << &stringREF << std::endl;
+
+    std::cout << "Valor da string: " << string << std::endl;
+    std::cout << "Valor apontado por stringPTR: " << *stringPTR << std::endl;
+    std::cout << "Valor referenciado por stringREF: " << stringREF << std::endl;
+
+    return 0;
+}
