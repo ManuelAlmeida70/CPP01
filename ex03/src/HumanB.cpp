61331d925cfb4101fbf5fd2b385c898aaadaa6fd
Manuel Mazunga De Almeida <maalmeid@c1r2s5.42luanda.com>
2025-05-30 15:34:06 +0100

    humanb done

--- ex03/src/HumanB.cpp
+++ ex03/src/HumanB.cpp
@@ -1,2 +1,17 @@
 #include "HumanB.hpp"
 
+HumanB::HumanB(std::string name) : name(name), weapon(NULL)
+{}
+
+void HumanB::setWeapon(Weapon& weapon)
+{
+    this->weapon = &weapon;
+}
+
+void HumanB::attack()
+{
+    if (this->weapon)
+        std::cout << this->name << " attacks with their " << this->weapon->getType();
+    else
+        std::cout << this->name << " attacks with their fists" << std::endl;
+}
